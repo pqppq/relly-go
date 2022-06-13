21c9593081cdfc5b2758d3fe5ea1d7bfc70cb274
pqppq <yutatsu0119@gmail.com>
2022-06-13 16:34:36 +0900

    fix page

--- src/page.go
+++ src/page.go
@@ -1,28 +1,24 @@
 package rellygo
 
 import (
-	"bytes"
 	"math"
 )
 
 type PageId uint64
-type Page struct {
-	buffer bytes.Buffer
-	pageId PageId
-}
 
 const PAGE_SIZE = 4096
-const INVALID_PAGE_ID PageId = math.MaxUint64
+const INVALID_PAGE_ID = PageId(math.MaxUint64)
 
-func New(n uint64) *Page {
+func (id *PageId) Valid() bool {
+	return *id != INVALID_PAGE_ID
+}
+
+type Page struct {
+	data []byte
+}
+
+func newPage() *Page {
 	p := new(Page)
-	if PageId(n) != INVALID_PAGE_ID {
-		p.pageId = PageId(n)
-	} else {
-		p.pageId = INVALID_PAGE_ID
-	}
-	buf := make([]byte, PAGE_SIZE)
-	p.buffer = *bytes.NewBuffer(buf)
-
+	p.data = make([]byte, PAGE_SIZE)
 	return p
 }
